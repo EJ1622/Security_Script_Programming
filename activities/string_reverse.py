cca699b6791f0875d9f0fb8b1997067a09fca662
El Jean L. Rementas <rementasej2002@gmail.com>
2026-01-21 09:44:51 -0500

    Created a string reversed python

--- activities/string_reverse.py
+++ activities/string_reverse.py
@@ -1,1 +1,12 @@
+# Week 2 Practice Activity 1
+user_input = input("Enter a string to reverse: ")
 
+# Reverse using slicing [start:stop:step] - step -1 goes backwards
+reversed_string = user_input[::-1]
+
+print(f"Original: {user_input}")
+print(f"Reversed: {reversed_string}")
+
+# How this works
+# ' input() ' captures user string
+# ' [::-1] ' slicing: start=end (implied), stop=0 (implied), step=-1 (backwards)
